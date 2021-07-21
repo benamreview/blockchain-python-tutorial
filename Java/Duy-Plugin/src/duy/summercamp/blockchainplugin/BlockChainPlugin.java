a1135f59142c91cc41d7879591c6e0cd68597954
Duy Ho <dhh3hb@mail.umkc.edu>
2021-07-21 04:40:22 -0500

    add block break event

--- Java/Duy-Plugin/src/duy/summercamp/blockchainplugin/BlockChainPlugin.java
+++ Java/Duy-Plugin/src/duy/summercamp/blockchainplugin/BlockChainPlugin.java
@@ -1,10 +1,22 @@
 package duy.summercamp.blockchainplugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.Base64;
 
 
-
-public class BlockChainPlugin extends JavaPlugin{
+public class BlockChainPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -54,7 +66,8 @@ public class BlockChainPlugin extends JavaPlugin{
         BlockListener blockListener = new BlockListener();
 
         PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(blockListener, this);
+//        pm.registerEvents(blockListener, this);
+        pm.registerEvents(this, this);
 
 
 
@@ -65,4 +78,76 @@ public class BlockChainPlugin extends JavaPlugin{
         //Fired when the server stops and disables all plugins
 
     }
+    @EventHandler
+    public void onPlayerBreak(BlockBreakEvent e) {
+        System.out.println("Block Break!");
+
+        Location blockl = e.getBlock().getLocation();
+        final Material block = e.getBlock().getType();
+
+        final org.bukkit.block.Block blockb = e.getPlayer().getWorld().getBlockAt(blockl);
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        if (block == Material.WARPED_WALL_SIGN) {
+            BlockState state = e.getBlock().getState();
+            Sign sign = (Sign) state;
+//            String tempData = sign.getLine(1);
+            Player player = e.getPlayer();
+            e.getPlayer().sendMessage(ChatColor.BLUE + "...Decrypting Messages in this Block...");
+            String data = sign.getLine(1);
+            player.sendMessage(ChatColor.YELLOW + "Data: " + data);
+            try {
+                byte[] decodedBytes = Base64.getDecoder().decode(data);
+                String decodedString = new String(decodedBytes);
+
+                if (decodedString.contains(player.getName())) {
+                    sign.setLine(1, decodedString);
+                    player.sendMessage(ChatColor.GREEN + "Decrypted Message: " + decodedString);
+                } else {
+                    player.sendMessage(ChatColor.RED + "This transaction is NOT for you!!!");
+
+                }
+            }
+            catch (Exception ex) {
+                System.out.println(ex);
+                player.sendMessage(ChatColor.RED + "This block does not contain an encrypted message or transaction!");
+            }
+            final String transactionData = sign.getLine(1);
+
+            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                @Override
+                public void run() {
+//                System.out.println("run!");
+                    blockb.setType(block);
+                    System.out.println(block);
+                    if (block == Material.WARPED_WALL_SIGN) {
+
+                        BlockState newState = e.getBlock().getState();
+                        Sign newSign = (Sign) newState;
+                        newSign.setLine(0, "Transaction:");
+                        newSign.setLine(1, transactionData);
+
+                        newSign.update();
+                        System.out.println(newSign.getLine(0));
+
+
+                    }
+                }
+            }, 10L);
+        }
+        else {
+            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                @Override
+                public void run() {
+//                System.out.println("run!");
+                    blockb.setType(block);
+                    System.out.println(block);
+                }
+            }, 10L);
+        }
+
+
+
+    }
+
+
 }
--- Java/Duy-Plugin/src/duy/summercamp/blockchainplugin/BlockListener.java
+++ Java/Duy-Plugin/src/duy/summercamp/blockchainplugin/BlockListener.java
@@ -1,5 +1,6 @@
 package duy.summercamp.blockchainplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
@@ -9,6 +10,7 @@ import org.bukkit.event.EventException;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.io.UnsupportedEncodingException;
@@ -27,15 +29,23 @@ public class BlockListener implements Listener {
                 BlockState state = block.getState();
                 Sign sign = (Sign) state;
                 String signline1 = sign.getLine(1);
-                player.sendMessage(ChatColor.YELLOW + "Message: " + signline1);
+                player.sendMessage(ChatColor.YELLOW + "Data: " + signline1);
+
 
                 try {
                     byte[] decodedBytes = Base64.getDecoder().decode(signline1);
                     String decodedString = new String(decodedBytes);
+                    System.out.println(block.getLocation());
+//                    player.getWorld().getBlockAt(block.getLocation()).setType(Material.BEDROCK);
+//                    Sign newSign = (Sign) player.getWorld().getBlockAt(block.getLocation()).getState();
+//                    newSign.setLine(0, "Transaction: ");
+
                     if (decodedString.contains(player.getName())) {
+                        sign.setLine(1, decodedString);
                         player.sendMessage(ChatColor.GREEN + "Decrypted Message: " + decodedString);
                     } else {
                         player.sendMessage(ChatColor.RED + "This transaction is NOT for you!!!");
+
                     }
                 }
                 catch (Exception e) {
@@ -45,10 +55,32 @@ public class BlockListener implements Listener {
 
 
             } else {
+//                block.setType(Material.BEDROCK);
                 player.sendMessage(
                         ChatColor.RED + "You clicked: " + ChatColor.RED + block.getType().toString().toUpperCase());
+
+
             }
         }
 //
     }
+//    @EventHandler
+//    public void onBlockBreak(BlockBreakEvent event)
+//    {
+//        System.out.println("Block break!");
+//        org.bukkit.block.Block b = event.getBlock();
+//        org.bukkit.Material b1 = b.getType();
+//        if (b1 == Material.DIAMOND_BLOCK  )
+//        {
+//            // Do you mean:
+//            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+//                @Override
+//                public void run() {
+//                    // Code to replace the block
+//                }
+//            }, 20L); // 20 ticks delay (20 ticks = 1 second)
+//            b.setType(Material.BEDROCK); // example to set it to log
+//            b.setCancelled
+//        }
+//    }
 }
